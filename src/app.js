d1bb56a24fed3dffe1a59004bea143b00201d09b
Florez264 <floresj10000@gmail.com>
2024-08-06 10:38:51 -0500

    galeria de fotos

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
-console.log("RUTA DESDE MI API ",path.join(__dirname, '..', 'uploads'));
 
 
 
